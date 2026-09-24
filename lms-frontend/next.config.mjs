0c1dfff515f4a47775d71a4a3dc97485005bdad9
ultimate-knight <maaz2607@gmail.com>
2026-09-24 06:41:23 +0530

    Describe your change

--- lms-frontend/next.config.mjs
+++ lms-frontend/next.config.mjs
@@ -1,6 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["localhost"],
+  allowedDevOrigins: ["localhost", "192.168.1.3"],
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
